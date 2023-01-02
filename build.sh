c6088bc4de640bb4faf154c52326fec14891c98f
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 17:33:03 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-if [ ! -d "build" ]; then
-    mkdir build
-fi
-cd build && cmake .. && make $*
+version https://git-lfs.github.com/spec/v1
+oid sha256:d69f7e105b308bcb993c13fe56bce28773beff9ccee38755b68666180e10e7f8
+size 87
